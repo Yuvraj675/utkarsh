ffbc4088424c421a9a74ee8753ef642845719db8
Yuvraj675 <yuviyg675@gmail.com>
2025-02-18 00:43:47 +0530

    big update to the site, completed the whole layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
   content: ["*.{html,js}"],
   theme: {
     extend: {
+      animation:{
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        }
+      }
     },
   },
   plugins: [
